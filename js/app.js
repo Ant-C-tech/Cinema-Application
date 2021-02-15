13eea71769e203901d1d6853f76afda3dbdda82d
Chyvikov-Anton <chivikovanton@gmail.com>
2021-02-15 14:22:52 +0200

    Add confirm ticket section with animation

---
+++ js/app.js
@@ -0,0 +1,42 @@
+"use strict";
+
+const buyTicketButton = document.querySelector("#buyTicket");
+const choosingSeatSection = document.querySelector("#choosingSeat");
+const confirmTicketSection = document.querySelector("#confirmTicket");
+
+const addClassToElement = (element, className) => {
+  element.classList.add(`${className}`);
+};
+
+const removeClassFromElement = (element, className) => {
+  element.classList.remove(`${className}`);
+};
+
+const showNextScreen = (currentElement, nextElement) => {
+  currentElement.addEventListener(
+    "animationend",
+    () => {
+      addClassToElement(currentElement, "d-none");
+      removeClassFromElement(currentElement, "hideAnimationForNextScreen");
+
+      removeClassFromElement(nextElement, "d-none");
+      addClassToElement(nextElement, "showAnimationForNextScreen");
+    },
+    { once: true }
+  );
+    nextElement.addEventListener(
+      "animationend",
+      () => {
+        removeClassFromElement(nextElement, "showAnimationForNextScreen");
+      },
+      { once: true }
+    );
+  addClassToElement(currentElement, "hideAnimationForNextScreen");
+};
+
+
+buyTicketButton.addEventListener("transitionend", (e) => e.stopPropagation());
+
+buyTicketButton.addEventListener("click", () => {
+  showNextScreen(choosingSeatSection, confirmTicketSection);
+})
