bda0b93e10229a1a1d80215a2b4c0e4b7009e80e
Chyvikov-Anton <chivikovanton@gmail.com>
2021-02-16 16:47:57 +0200

    Add mobile optimization for ticket

--- js/app.js
+++ js/app.js
@@ -1,8 +1,12 @@
 "use strict";
 
-const buyTicketButton = document.querySelector("#buyTicket");
+const body = document.querySelector("body");
 const choosingSeatSection = document.querySelector("#choosingSeat");
 const confirmTicketSection = document.querySelector("#confirmTicket");
+const orderContainer = document.querySelector("#order");
+
+const buyTicketButton = document.querySelector("#buyTicket");
+
 const movieTitle = document.querySelector("#movieTitle");
 
 const addClassToElement = (element, className) => {
@@ -13,7 +17,7 @@ const removeClassFromElement = (element, className) => {
   element.classList.remove(`${className}`);
 };
 
-const showNextScreen = (currentElement, nextElement) => {
+const showNextScreen = (currentElement, nextElement, callback) => {
   currentElement.addEventListener(
     "animationend",
     () => {
@@ -25,25 +29,38 @@ const showNextScreen = (currentElement, nextElement) => {
     },
     { once: true }
   );
-    nextElement.addEventListener(
-      "animationend",
-      () => {
-        removeClassFromElement(nextElement, "showAnimationForNextScreen");
-      },
-      { once: true }
-    );
+  nextElement.addEventListener(
+    "animationend",
+    () => {
+      removeClassFromElement(nextElement, "showAnimationForNextScreen");
+      callback && callback();
+    },
+    { once: true }
+  );
   addClassToElement(currentElement, "hideAnimationForNextScreen");
 };
 
-const fillTicketWithData = () => {
+const changeVisualizationAfterConfirmTicketSectionOverflow = () => {
+  const getWindowHeight = document.documentElement.clientHeight;
+  const getConfirmTicketSectionHeight = confirmTicketSection.offsetHeight;
 
-}
+  if (getWindowHeight > getConfirmTicketSectionHeight) {
+    removeClassFromElement(body, "h-auto");
+  } else {
+    addClassToElement(body, "h-auto");
+  }
+};
 
+const fillTicketWithData = () => {};
 
 buyTicketButton.addEventListener("transitionend", (e) => e.stopPropagation());
 
 buyTicketButton.addEventListener("click", () => {
-  showNextScreen(choosingSeatSection, confirmTicketSection);
-})
+  showNextScreen(
+    choosingSeatSection,
+    confirmTicketSection,
+    changeVisualizationAfterConfirmTicketSectionOverflow
+  );
+});
 
-//Если высота модального окна больше высоты боди - делать боде height:auto;
+//Если высота модального окна больше высоты боди - делать боди height:auto;
